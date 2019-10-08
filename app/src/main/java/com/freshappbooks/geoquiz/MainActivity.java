a10b75fb51b1b397a0b1a2180539b225b4b7e15f
vjmarcus <vjmarcus1@gmail.com>
2019-10-08 20:17:13 +0300

    Add Question class

--- app/src/main/java/com/freshappbooks/geoquiz/MainActivity.java
+++ app/src/main/java/com/freshappbooks/geoquiz/MainActivity.java
@@ -3,12 +3,50 @@ package com.freshappbooks.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonTrue;
+    private Button buttonFalse;
+    private Button buttonPrev;
+    private Button buttonNext;
+    private TextView questionTextView;
+    private int currentIndex = 0;
+    private Question [] questionBook = new Question[] {
+      new Question(R.string.question_australia, true),
+      new Question(R.string.question_oceans, true),
+      new Question(R.string.question_mideast, false),
+      new Question(R.string.question_africa, false),
+      new Question(R.string.question_americas, true),
+      new Question(R.string.question_asia, true),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTrue = findViewById(R.id.btn_true);
+        buttonFalse = findViewById(R.id.btn_false);
+        questionTextView = findViewById(R.id.question_tv);
+        int question = questionBook[currentIndex].getTextResId();
+        questionTextView.setText(question);
+
+        buttonTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
